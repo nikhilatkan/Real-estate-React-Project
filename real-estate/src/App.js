63d3c86843da82c0b6395b268cdf07898339a357
kunalrajput22 <kunalrajput221098@gmail.com>
2024-05-24 12:32:07 +0530

    header component created

--- real-estate/src/App.js
+++ real-estate/src/App.js
@@ -6,10 +6,12 @@ import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
+import Header from "./components/Header";
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
---
+++ real-estate/src/components/Header.jsx
@@ -0,0 +1,55 @@
+import { useLocation, useNavigate } from "react-router";
+
+function Header() {
+  const location = useLocation();
+  console.log(location.pathname);
+
+  const navigate = useNavigate();
+
+  function pathMatchRoute(route) {
+    if (location.pathname === route) return true;
+  }
+
+  return (
+    <div className="headerContainer">
+      <header className="headerSection">
+        <div>
+          <img
+            className="logo-image"
+            src="https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg"
+            alt="logo"
+            onClick={() => navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="naviLinks">
+            <li
+              className={`navlink ${pathMatchRoute("/") && "active-link"}`}
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`navlink ${
+                pathMatchRoute("/offers") && "active-link"
+              }`}
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`navlink ${
+                pathMatchRoute("/sign-in") && "active-link"
+              }`}
+              onClick={() => navigate("/sign-in")}
+            >
+              Sign in
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
