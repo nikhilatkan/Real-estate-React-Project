99ce8a28b3ab925d7ce0f8acde3442faebef6f88
kunalrajput22 <kunalrajput221098@gmail.com>
2024-05-24 09:32:36 +0530

    created routes and pages

--- real-estate/src/App.js
+++ real-estate/src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ real-estate/src/pages/ForgotPassword.jsx
@@ -0,0 +1,5 @@
+function ForgotPassword() {
+  return <div>Forgot password</div>;
+}
+
+export default ForgotPassword;
