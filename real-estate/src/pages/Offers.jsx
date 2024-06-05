a29b9ca54fab446db15f1a60f0f60649ef1aa653
kunalrajput22 <kunalrajput221098@gmail.com>
2024-06-06 01:33:49 +0530

    dev branch added

--- real-estate/src/pages/Offers.jsx
+++ real-estate/src/pages/Offers.jsx
@@ -1,5 +1,5 @@
 function Offers() {
-  return <div>Offers</div>;
+  return <div>Offerss</div>;
 }
 
 export default Offers;
--- real-estate/src/pages/SignIn.jsx
+++ real-estate/src/pages/SignIn.jsx
@@ -1,5 +1,23 @@
 function SignIn() {
-  return <div>Sign In</div>;
+  return (
+    <section className="sign-in-heading">
+      <h1>Sign in</h1>
+      <div>
+        <div className="signin-img-container">
+          <img
+            className="signin-img"
+            src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="key"
+          />
+        </div>
+        <div>
+          <form>
+            <input type="text" />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default SignIn;
